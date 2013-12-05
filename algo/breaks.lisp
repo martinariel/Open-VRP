8f666d0ff689eb95bf81827886c2a0578372ac7a
Marc Kuo <kuomarc2@gmail.com>
2013-12-05 15:10:16 -0800

    bvrptw-specific feasible-move-p and assess-move

---
+++ algo/breaks.lisp
@@ -0,0 +1,88 @@
+(in-package :open-vrp.algo)
+
+;;; Break specific algo tools
+;;; ----------------
+;;; 0. feasible-move-p
+;;; 1. assess-move
+;;; 2. perform-move
+;;; 3. constraints checking
+
+(defmethod feasible-move-p and ((sol BVRPTW) (m insertion-move))
+  (with-slots (node-id vehicle-id index) m
+    (let* ((v (vehicle sol vehicle-id))
+           (route (vehicle-route v))
+           (ins-node (visit-node sol node-id)))
+      (labels ((get-location (node)
+                 (if (pitstop-p node)
+                     (pitstop-break-location node)
+                     (visit-node-id node)))
+               (iter (route time loc i)
+                 (if (and (null route) (< i 0))
+                     (values (<= (+ time (travel-time loc (vehicle-end-location v) (problem-dist-matrix sol) :speed (vehicle-speed v)))
+                                 (vehicle-shift-end v))
+                             time loc)
+                     (let* ((to (if (= 0 i) ins-node (car route)))
+                            (arr-time (+ time (travel-time loc (get-location to) (problem-dist-matrix sol)))))
+                       (and
+                        (<= arr-time (visit-end to))
+                        (iter (if (= 0 i) route (cdr route))
+                              (time-after-visit to arr-time)
+                              (get-location to)
+                              (1- i)))))))
+        (iter route (vehicle-shift-start v) (vehicle-start-location v) index)))))
+
+
+(defmethod assess-move ((sol bvrptw) (m insertion-move))
+  "BVRPTW specific assess-move for insertions, which updates the break location if it is being inserted next to it.
+
+      Cases of inserting X:
+      If normal, just calculate insertion cost of X
+
+      If node-after is a pitstop:
+        - subtract node-before -> current pitstop
+        - subtract current pitstop -> node-after-pitstop
+        - add node-before -> location X
+        - update pitstop location with closest-between X and node-after-pitstop
+        - ^ will return detour distance
+
+      If node-before is a pitstop:
+        - subtract node-before -> current pitstop
+        - subtract current pitstop -> node-after-pitstop
+        - add location X -> node-after
+        - update pitstop location with closest-between node-before-pitstop and X
+        - ^ will return detour distance"
+  (with-slots (node-id vehicle-id index) m
+    (let* ((veh (vehicle sol vehicle-id))
+           (route (vehicle-route veh))
+           (dist-matrix (problem-dist-matrix sol))
+           (node-before (if (= index 0)
+                            (vehicle-start-location veh)
+                            (nth (1- index) route)))
+           (node-after (if (= index (length route))
+                              (vehicle-end-location veh)
+                              (nth index route)))
+           (node-before-id (if (symbolp node-before) node-before (visit-node-id node-before)))
+           (node-after-id (if (symbolp node-after) node-after (visit-node-id node-after))))
+      (cond ((pitstop-p node-after)
+             (let* ((pit (pitstop-break-location node-after))
+                    (node-after-pit (if (= (1+ index) (length route))
+                                        (vehicle-end-location veh)
+                                        (visit-node-id (nth (1+ index) route))))
+                    (dist-to-pit (distance node-before-id pit dist-matrix)) ;subtract
+                    (dist-from-pit (distance pit node-after-pit dist-matrix)) ;subtract
+                    (dist-to-new (distance node-before-id node-id dist-matrix))) ;add
+               (multiple-value-bind (loc dist-detour)
+                   (closest-between node-id node-after-pit dist-matrix (problem-break-locations sol))
+                 (setf (move-fitness m)
+                       (handler-case
+                           (- (+ dist-detour dist-to-new)
+                              (+ dist-to-pit dist-from-pit))
+                         (distance-between-nodes-undefined () nil))))))
+            ((pitstop-p node-before) :todo)
+            (t (setf (move-fitness m)
+                     (handler-case
+                         (-
+                          (+ (distance node-before-id node-id dist-matrix)
+                             (distance node-id node-after-id dist-matrix))
+                          (distance node-before-id node-after-id dist-matrix))
+                       (distance-between-nodes-undefined () nil))))))))
