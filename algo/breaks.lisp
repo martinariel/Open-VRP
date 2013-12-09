61b68d659a51bdb3240e38ca4e3b87242e5dfddd
Marc Kuo <kuomarc2@gmail.com>
2013-12-08 16:11:59 -0800

    implement specialized assess-move for TS-best-insertion-move on bvrptw, to take into account updating breaks if the node to be removed is next to a break

--- algo/breaks.lisp
+++ algo/breaks.lisp
@@ -54,63 +54,61 @@
         - add location X -> node-after
         - update pitstop location with closest-between node-before-pitstop and X
         - ^ will return detour distance"
-  (if (eq (type-of m) 'TS-best-insertion-move)
-      (call-next-method)
-      (with-slots (node-id vehicle-id index) m
-        (let* ((veh (vehicle sol vehicle-id))
-               (route (vehicle-route veh))
-               (dist-matrix (problem-dist-matrix sol))
-               (node-before (if (= index 0)
-                                (vehicle-start-location veh)
-                                (nth (1- index) route)))
-               (node-after (if (= index (length route))
-                               (vehicle-end-location veh)
-                               (nth index route)))
-               (node-before-id (if (symbolp node-before) node-before (visit-node-id node-before)))
-               (node-after-id (if (symbolp node-after) node-after (visit-node-id node-after))))
-          (cond ((pitstop-p node-after)
-                 (let* ((pit (pitstop-break-location node-after))
-                        (node-after-pit (if (= (1+ index) (length route))
-                                            (vehicle-end-location veh)
-                                            (visit-node-id (nth (1+ index) route))))
-                        (dist-to-pit (distance node-before-id pit dist-matrix)) ;subtract
-                        (dist-from-pit (distance pit node-after-pit dist-matrix)) ;subtract
-                        (dist-to-new (distance node-before-id node-id dist-matrix))) ;add
-                   (multiple-value-bind (loc dist-detour)
-                       (closest-between node-id node-after-pit dist-matrix (problem-break-locations sol))
-                     (declare (ignore loc))
-                     (setf (move-fitness m)
-                           (handler-case
-                               (- (+ dist-detour dist-to-new)
-                                  (+ dist-to-pit dist-from-pit))
-                             (distance-between-nodes-undefined () nil))))))
-                ((pitstop-p node-before)
-                 (let* ((pit (pitstop-break-location node-before))
-                        (node-before-pit (if (= index 1)
-                                             (vehicle-start-location veh)
-                                             (visit-node-id (nth (- index 2) route))))
-                        (dist-to-pit (distance node-before-pit pit dist-matrix)) ;subtract
-                        (dist-from-pit (distance pit node-after-id dist-matrix)) ;subtract
-                        (dist-to-new (distance node-id node-after-id dist-matrix))) ;add
-                   ;; (format t "pit: ~A~% node-before-pit: ~A~% dist-to-pit: ~A~% dist-from-pit: ~A~% dist-to-new:~A" pit node-before-pit dist-to-pit dist-from-pit dist-to-new)
-                   (multiple-value-bind (loc dist-detour)
-                       (closest-between node-before-pit node-id dist-matrix (problem-break-locations sol))
-                     (declare (ignore loc))
-                     ;; (format t "loc: ~A~% dist-detour: ~A~%" loc dist-detour)
-                     ;; (print (- (+ dist-detour dist-to-new)
-                     ;;              (+ dist-to-pit dist-from-pit)))
-                     (setf (move-fitness m)
-                           (handler-case
-                               (- (+ dist-detour dist-to-new)
-                                  (+ dist-to-pit dist-from-pit))
-                             (distance-between-nodes-undefined () nil))))))
-                (t (setf (move-fitness m)
-                         (handler-case
-                             (-
-                              (+ (distance node-before-id node-id dist-matrix)
-                                 (distance node-id node-after-id dist-matrix))
-                              (distance node-before-id node-after-id dist-matrix))
-                           (distance-between-nodes-undefined () nil)))))))))
+  (with-slots (node-id vehicle-id index) m
+    (let* ((veh (vehicle sol vehicle-id))
+           (route (vehicle-route veh))
+           (dist-matrix (problem-dist-matrix sol))
+           (node-before (if (= index 0)
+                            (vehicle-start-location veh)
+                            (nth (1- index) route)))
+           (node-after (if (= index (length route))
+                           (vehicle-end-location veh)
+                           (nth index route)))
+           (node-before-id (if (symbolp node-before) node-before (visit-node-id node-before)))
+           (node-after-id (if (symbolp node-after) node-after (visit-node-id node-after))))
+      (cond ((pitstop-p node-after)
+             (let* ((pit (pitstop-break-location node-after))
+                    (node-after-pit (if (= (1+ index) (length route))
+                                        (vehicle-end-location veh)
+                                        (visit-node-id (nth (1+ index) route))))
+                    (dist-to-pit (distance node-before-id pit dist-matrix)) ;subtract
+                    (dist-from-pit (distance pit node-after-pit dist-matrix)) ;subtract
+                    (dist-to-new (distance node-before-id node-id dist-matrix))) ;add
+               (multiple-value-bind (loc dist-detour)
+                   (closest-between node-id node-after-pit dist-matrix (problem-break-locations sol))
+                 (declare (ignore loc))
+                 (setf (move-fitness m)
+                       (handler-case
+                           (- (+ dist-detour dist-to-new)
+                              (+ dist-to-pit dist-from-pit))
+                         (distance-between-nodes-undefined () nil))))))
+            ((pitstop-p node-before)
+             (let* ((pit (pitstop-break-location node-before))
+                    (node-before-pit (if (= index 1)
+                                         (vehicle-start-location veh)
+                                         (visit-node-id (nth (- index 2) route))))
+                    (dist-to-pit (distance node-before-pit pit dist-matrix)) ;subtract
+                    (dist-from-pit (distance pit node-after-id dist-matrix)) ;subtract
+                    (dist-to-new (distance node-id node-after-id dist-matrix))) ;add
+               ;; (format t "pit: ~A~% node-before-pit: ~A~% dist-to-pit: ~A~% dist-from-pit: ~A~% dist-to-new:~A" pit node-before-pit dist-to-pit dist-from-pit dist-to-new)
+               (multiple-value-bind (loc dist-detour)
+                   (closest-between node-before-pit node-id dist-matrix (problem-break-locations sol))
+                 (declare (ignore loc))
+                 ;; (format t "loc: ~A~% dist-detour: ~A~%" loc dist-detour)
+                 ;; (print (- (+ dist-detour dist-to-new)
+                 ;;              (+ dist-to-pit dist-from-pit)))
+                 (setf (move-fitness m)
+                       (handler-case
+                           (- (+ dist-detour dist-to-new)
+                              (+ dist-to-pit dist-from-pit))
+                         (distance-between-nodes-undefined () nil))))))
+            (t (setf (move-fitness m)
+                     (handler-case
+                         (-
+                          (+ (distance node-before-id node-id dist-matrix)
+                             (distance node-id node-after-id dist-matrix))
+                          (distance node-before-id node-after-id dist-matrix))
+                       (distance-between-nodes-undefined () nil))))))))
 
 (defmethod perform-move ((sol bvrptw) (m insertion-move))
   "Checks if move is next to a break location, if so, update break location"
@@ -141,3 +139,102 @@
                            (closest-between node-before-pit node-id dist-matrix (problem-break-locations sol)))
                      (call-next-method)))
                   (t (call-next-method))))))))
+
+(defmethod assess-move ((sol bvrptw) (mv TS-best-insertion-move))
+  (with-slots (node-id vehicle-id fitness) mv
+    (flet ((insertion-cost (veh-id node-id)
+             "Cost of inserting node-id into veh-id, calculated using best-insertion-move, unless veh-id is :UNSERVED"
+             (if (eq :UNSERVED veh-id)
+                 *unserved-penalty*
+                 (move-fitness (get-best-insertion-move-in-vehicle sol veh-id node-id)))))
+      (let ((v-w-n-id (vehicle-with-node-id sol node-id)))
+        (handler-case
+            ;; Calculate move differently when it involves the :UNSERVED list
+            (if (eq :UNSERVED v-w-n-id)
+                (setf fitness (- (insertion-cost vehicle-id node-id) *unserved-penalty*))
+                (let* ((dist-matrix (problem-dist-matrix sol))
+                       (veh (vehicle sol v-w-n-id))
+                       (route (vehicle-route veh))
+                       (pos (position node-id route :key #'visit-node-id))
+                       (node-before (if (= pos 0) (vehicle-start-location veh) (nth (1- pos) route)))
+                       (node-after (if (= (1+ pos) (length route)) (vehicle-end-location veh) (nth (1+ pos) route)))
+                       (node-before-id (if (symbolp node-before) node-before (visit-node-id node-before)))
+                       (node-after-id (if (symbolp node-after) node-after (visit-node-id node-after))))
+                  (cond ((pitstop-p node-after)
+                                        ; if node-after the one being removed is a pitstop, update that location by calculating:
+                                        ;  [0 - 1 - B - 2 - 3]
+                                        ; + calculate new B between 0 and 2
+                                        ; - remove leg 1-B and B-2
+                                        ; - remove leg 0-1
+                         (let* ((pit (pitstop-break-location node-after))
+                                (node-after-pit (if (= (+ 2 pos) (length route))
+                                                    (vehicle-end-location veh)
+                                                    (visit-node-id (nth (+ 2 pos) route))))
+                                (dist-before (distance node-before node-id dist-matrix)) ;subtract
+                                (dist-to-pit (distance node-id pit dist-matrix)) ;subtract
+                                (dist-from-pit (distance pit node-after-pit dist-matrix))) ;subtract
+                           (multiple-value-bind (loc dist-detour)
+                               (closest-between node-before node-after-pit dist-matrix (problem-break-locations sol))
+                             (declare (ignore loc))
+                             (setf fitness
+                                   (handler-case
+                                       (- (insertion-cost vehicle-id node-id)
+                                        ;save by removing:
+                                          (- dist-detour ;   new B between 0 and 2
+                                             dist-before ;   0-1
+                                             dist-to-pit ;   1-B
+                                             dist-from-pit)) ; B-2
+                                     (distance-between-nodes-undefined () nil))))))
+                        ((pitstop-p node-before)
+                                        ; if node-before the one being removed is a pitstop, update that location by calculating:
+                                        ;  [1 - B - 2 - 3]
+                                        ; + calculate new B between 1 and 3
+                                        ; - remove leg 1-B and B-2
+                                        ; - remove leg 2-3
+                         (let* ((pit (pitstop-break-location node-before))
+                                (node-before-pit (if (= pos 1)
+                                                     (vehicle-start-location veh)
+                                                     (visit-node-id (nth (- pos 2) route))))
+                                (dist-to-pit (distance node-before-pit pit dist-matrix)) ;subtract 1-B
+                                (dist-from-pit (distance pit node-id dist-matrix)) ;subtract B-2
+                                (dist-after (distance node-id node-after-id dist-matrix))) ;subtract 2-3
+                           ;; (format t "pit: ~A~% node-before-pit: ~A~% dist-to-pit: ~A~% dist-from-pit: ~A~% dist-after:~A~%" pit node-before-pit dist-to-pit dist-from-pit dist-after)
+                           (multiple-value-bind (loc dist-detour)
+                               (closest-between node-before-pit node-after-id dist-matrix (problem-break-locations sol))
+                             (declare (ignore loc))
+                             ;; (format t "loc: ~A~% dist-detour: ~A~%" loc dist-detour)
+                             ;; (print (insertion-cost vehicle-id node-id))
+                             ;; (print (- (insertion-cost vehicle-id node-id)
+                             ;;           (- dist-detour
+                             ;;                 dist-to-pit
+                             ;;                 dist-from-pit
+                             ;;                 dist-after)))
+                             (setf fitness
+                                   (handler-case
+                                       (+ (insertion-cost vehicle-id node-id)
+                                        ;cost of removing:
+                                          (- dist-detour
+                                             dist-to-pit
+                                             dist-from-pit
+                                             dist-after))
+                                     (distance-between-nodes-undefined () nil))))))
+                        (t
+                                        ; By default, just calculate savings:
+                                        ; [1 - 2 - 3]
+                                        ; subtract 1-2 and 2-3
+                                        ; add 1-3
+                         (setf fitness
+                               (handler-case
+                                   (- (insertion-cost vehicle-id node-id)
+                                        ;save by removing:
+                                      (let ((dist-before (handler-case (distance node-before-id node-id dist-matrix)
+                                                           (same-origin-destination () 0))) ; 1-2
+                                            (dist-after (handler-case (distance node-id node-after-id dist-matrix)
+                                                          (same-origin-destination () 0))) ; 2-3
+                                            (dist-between (handler-case (distance node-before-id node-after-id dist-matrix)
+                                                            (same-origin-destination () 0)))) ; 1-3
+                                        (- dist-between
+                                           dist-after
+                                           dist-before)))
+                                 (distance-between-nodes-undefined () nil)))))))
+          (no-feasible-move () (setf fitness nil)))))))
--- test/algo-breaks.lisp
+++ test/algo-breaks.lisp
@@ -57,6 +57,31 @@
     (assert-equal nil (assess-move bvrptw (make-insertion-move :node-id :o5 :vehicle-id :t1 :index 3)))
     (assert-error 'simple-error (perform-move bvrptw (make-insertion-move :node-id :o5 :vehicle-id :t1 :index 3)))))
 
+(define-test assess-insertion-moves-TS-moves-with-breaks
+  (:tag :break-debug)
+  "Test assess-move while checking tw and updating breaks"
+  (let* ((o1 (make-order :duration 1 :start 0 :end 15 :node-id :o1))
+         (o2 (make-order :duration 2 :start 0 :end 20 :node-id :o2))
+         (o3 (make-order :duration 3 :start 10 :end 25 :node-id :o3))
+         (o4 (make-order :duration 4 :start 10 :end 20 :node-id :o4))
+         (o5 (make-order :duration 5 :start 0 :end 15 :node-id :o5))
+         (b1 (make-pitstop :duration 5 :start 10 :end 15 :node-id :b1 :break-type "long" :break-location :o5))
+         (b2 (make-pitstop :duration 5 :start 10 :end 25 :node-id :b2 :break-type "long" :break-location :o3))
+         (t1 (make-vehicle :id :t1 :route (list o4 b1) :start-location :A :end-location :B :shift-end 25))
+         (t2 (make-vehicle :id :t2 :route (list o1 b2 o2 o3) :start-location :A :end-location :B :shift-start 0 :shift-end 30))
+         (dist {:o1 {      :o2 1 :o3 2 :o4 3 :o5 5 :A 1 :B 4}
+                :o2 {:o1 1       :o3 1 :o4 2 :o5 4 :A 2 :B 3}
+                :o3 {:o1 2 :o2 1       :o4 1 :o5 3 :A 3 :B 2}
+                :o4 {:o1 3 :o2 2 :o3 1       :o5 1 :A 4 :B 1}
+                :o5 {:o1 4 :o2 3 :o3 2 :o4 1       :A 6 :B 2}
+                :A  {:o1 1 :o2 2 :o3 3 :o4 4 :o5 6      :B 5}
+                :B  {:o1 4 :o2 3 :o3 2 :o4 1 :o5 2 :A 5     }})
+         (bvrptw (make-instance 'bvrptw :fleet (list t1 t2)
+                                :break-locations '(:o5 :o3)
+                                :dist-matrix dist
+                                :visits {:o1 o1 :o2 o2 :o3 o3 :o4 o4 :o5 o5}))
+         (move (make-TS-best-insertion-move :node-id :o2 :vehicle-id :t1)))
+    (assert-equal 3 (assess-move bvrptw move))))
 
 (define-test tabu-search-with-breaks
   (:tag :break-algo)
@@ -80,9 +105,9 @@
                                 :dist-matrix dist
                                 :visits {:o1 o1 :o2 o2 :o3 o3 :o4 o4}))
          (init-sol (solve-prob bvrptw (make-instance 'greedy-best-insertion)))
-         (ts (solve-prob bvrptw (make-instance 'tabu-search))))
+         (ts (solve-prob bvrptw (make-instance 'tabu-search :iterations 100))))
     (assert-true (< (algo-best-fitness init-sol) 100))
     (assert-equal 8 (length (generate-moves ts)))
-    (assert-equal 12 (algo-best-fitness ts))
-    (assert-equal '((:A :BREAK :O2 :O1 :A) (:B :BREAK :O4 :O3 :B))
+    (assert-equal 8 (algo-best-fitness ts))
+    (assert-equal '((:A :O1 :BREAK :O4 :O3 :O2 :A) (:B :BREAK :B))
                   (route-indices (algo-best-sol ts)))))
